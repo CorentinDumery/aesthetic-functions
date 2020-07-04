ec225e0068c42f0c88f54a225c8a627bfde21e0c
drlisette <32973223+drlisette@users.noreply.github.com>
2020-07-04 20:36:16 +0800

    solve exit code=1

--- colorExplorer.py
+++ colorExplorer.py
@@ -61,8 +61,32 @@ def func(xx,yy, p1, p2, offx, offy, f=""):
     
     
     if f != "":
+        f = f.replace("α",str(p1))
+        f = f.replace("β",str(p2))
         return eval(f)
 
+<<<<<<< HEAD
+    return 10000*np.cos( ((i+j)**2)*100/p1 )*np.sin(((i-j)**2)*100/p1) #quadrillage
+    return 255*np.sin( ((dh1)**2 +(dh1b)**2)*p1/1 ) #vinyl
+    return scale*np.cos(p1*xx)*np.exp(np.sin(p2*xx*yy)) #lines with dots
+    return scale*( (1-np.exp(-(dh1**2+dh1b**2)/p1)) ) #other sparkling sun
+    return 255*np.sin((i+j)*p1/20+np.cos((i-j)*p2/20)*5) #wavies
+    #return scale*np.sin(dh1*dh1b)**2
+    return 255*255*np.deg2rad(i*j**2)
+    return np.power(((dh1+dh1b)**2*10*p1).astype(int),((dh1-dh1b)**2*10*p2).astype(int))
+    return np.bitwise_xor(((dh1+dh1b)**2*10*p1).astype(int),((dh1-dh1b)**2*10*p2).astype(int)) #kilts
+    return np.bitwise_or(((dh1+dh1b)**2*10*p1).astype(int),((dh1-dh1b)**2*10*p2).astype(int)) #cardigan
+    
+    return np.bitwise_and((i*j*10*p1).astype(int),((dh1*dh1b)*10*p2).astype(int))
+    
+    return ( 255*np.sin(i*j*100/p1)*np.sin((i**3-j**2)*100/p2))
+    
+    return 255*np.sin((i+j*1.8)**2*p1/50+np.cos((i-j*1.8)**2*p2/100)*5)
+    return np.bitwise_and(((dh1+dh1b)**2*10*p1).astype(int),((dh1-dh1b)**2*10*p2).astype(int)) #red minecraft
+    
+    return p1*np.cos(np.sin(i*10/p2))+p1*np.cos(np.sin(j*10/p2)) #grandma's wallpaper
+    return (i-j*p2/50)*(i+j*p2/50)*i*(j*p2/50)*np.exp(p1/100)/10 #starry star
+=======
     # return 255*np.sin((i+j)*p1/20+np.cos((i-j)*p2/20)*5) #wavies
     # #return scale*np.sin(dh1*dh1b)**2
     # return 255*255*np.deg2rad(i*j**2)
@@ -80,12 +104,26 @@ def func(xx,yy, p1, p2, offx, offy, f=""):
     # return 10000*np.cos( ((i+j)**2)*100/p1 )*np.sin(((i-j)**2)*100/p1) #quadrillage
     # return p1*np.cos(np.sin(i*10/p2))+p1*np.cos(np.sin(j*10/p2)) #grandma's wallpaper
     # return (i-j*p2/50)*(i+j*p2/50)*i*(j*p2/50)*np.exp(p1/100)/10 #starry star
+>>>>>>> solve exit code=1
     
     # return (scale*np.sin( (i*22+np.cos(j*4)*5)/10)*(scale*np.sin( (i*22+np.cos(j*4)*5)/10) > 0)   *np.exp((-(((i+45/100)*3)**10))/1000)*np.exp((-(((j-23/100)*4)**10))/1000)  + scale*np.sin( ((-i+p1/100)*22+np.cos(j*4)*5)/10)*(scale*np.sin( ((-i+p1/100)*22+np.cos(j*4)*5)/10) > 0)*np.exp((-((((-i+p1/100)+45/100)*3)**10))/1000)*np.exp((-(((j-23/100)*4)**10))/1000))/(p2/1)
     # above: heart
     # return scale*np.sin(xx*10+np.cos(yy*10)*5) #waves
     # return scale*(np.exp(-(i**2)*p1) + np.exp(-((i-1)**2)*p1) + np.exp(-(j**2)*p1) + np.exp(-((j-1)**2)*p1)) #squares
     return scale*np.log( p1*i**2 + p1*j**2 +1+p2 ) #dongdong's log
+<<<<<<< HEAD
+    return scale*(np.sin(dh1)+np.cos(dh1b)*p2)**p1 #surf, high beta
+    return scale*np.exp(-p1*xx/(0.1+np.cos(yy*p2))) #fangs
+    
+    return (dh1*100*p1)**2 + (dh1b *100*p2)**2 #blobs
+    return scale*np.exp(-dh1*dh1b*p1-dh1*dh1*p2) #cross
+    return scale*(np.sin(dh1)+np.cos(dh1b)*p2)**p1 #eye?
+    return (i**2 + j**2)*1000 #sparkling sun
+    return ((np.sin((dh1+dh1b)**2*p1)*200+np.cos(dh1b*p2)*200)) #boutons disco
+    return scale*i*p1/1000+p2*scale*np.cos(j) #wavy
+    return scale*np.sin(p1*i**2 + j**2)*np.cos(p2*i*j) #damier
+    return 1 #pear of illusions (see image)
+=======
     # return scale*(np.sin(dh1)+np.cos(dh1b)*p2)**p1 #surf, high beta
     # return scale*np.exp(-p1*xx/(0.1+np.cos(yy*p2))) #fangs
     # return scale*np.cos(p1*xx)*np.exp(np.sin(p2*xx*yy)) #lines with dots
@@ -97,6 +135,7 @@ def func(xx,yy, p1, p2, offx, offy, f=""):
     # return scale*i*p1/1000+p2*scale*np.cos(j) #wavy
     # return scale*np.sin(p1*i**2 + j**2)*np.cos(p2*i*j) #damier
     # return 1 #pear of illusions (see image)
+>>>>>>> solve exit code=1
 
 
 class GUI():
@@ -184,6 +223,22 @@ class GUI():
         self.bfj = tk.Button(self.formulaFrame, text='j', command= lambda: self.addFormula("j"), padx=5)
         self.bfj.grid(row=1, column=2)
         
+<<<<<<< HEAD
+        self.bfalpha = Button(self.formulaFrame, text='α', command= lambda: self.addFormula("α"))
+        self.bfalpha.grid(row=1, column = 3)
+        
+        self.bfbeta = Button(self.formulaFrame, text='β', command= lambda: self.addFormula("β"))
+        self.bfbeta.grid(row=1, column = 4)
+        
+        self.bf1 = Button(self.formulaFrame, text='exp', command= lambda: self.addFormula("exp"))
+        self.bf1.grid(row=1, column = 5)
+        
+        self.bf2 = Button(self.formulaFrame, text='cos', command= lambda: self.addFormula("cos"))
+        self.bf2.grid(row=1, column = 6)
+        
+        self.bf3 = Button(self.formulaFrame, text='sin', command= lambda: self.addFormula("sin"))
+        self.bf3.grid(row=1, column = 7)
+=======
         self.bf1 = tk.Button(self.formulaFrame, text='exp', command= lambda: self.addFormula("exp"))
         self.bf1.grid(row=1, column=3)
         
@@ -192,6 +247,7 @@ class GUI():
         
         self.bf3 = tk.Button(self.formulaFrame, text='sin', command= lambda: self.addFormula("sin"))
         self.bf3.grid(row=1, column=5)
+>>>>>>> solve exit code=1
         
         self.formulaEntry = tk.Entry(self.formulaFrame, textvariable=self.formula, width=60)
         self.formulaEntry.grid(row=2, column=1, columnspan=5)
@@ -327,7 +383,7 @@ class GUI():
         pos = self.formulaEntry.index(tk.INSERT)
         if string in ["exp","cos","sin"]:
             txt = txt[:pos] + "np."+string+"()" + txt[pos:]
-        if string in ["i","j"]:
+        if string in ["i","j","α","β"]:
             txt += string
         self.formula.set( txt )
         print(self.formula.get())
@@ -373,4 +429,8 @@ class GUI():
   
 
 
+<<<<<<< HEAD
+root.quit()
+=======
 app = GUI()
+>>>>>>> solve exit code=1
